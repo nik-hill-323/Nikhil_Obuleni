761985000d4d2aaba277597cf2f5e3abe2b30b5a
Nikhil O <106472774+sheeeeeeeeh@users.noreply.github.com>
2025-10-08 21:28:23 -0400

    Configure for mass123 deployment
    ✨ Portfolio ready to deploy at:
    https://nik-hill-323.github.io/mass123/

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/mass123' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
